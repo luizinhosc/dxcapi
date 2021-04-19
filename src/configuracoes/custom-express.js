b8bfc2d9cf16766a8181d6fa6c53e42216ad90cd
luizinhosc <luizinhoanselmo@gmail.com>
2021-04-19 11:50:05 -0300

    conf heroku

--- src/configuracoes/custom-express.js
+++ src/configuracoes/custom-express.js
@@ -5,10 +5,10 @@ const roteador = require('../app/roteadores')
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.set('segredoJwt','secret')
-app.set('crypto', 'segredoDeEstado')
-app.set('chaveAcessoTemperatura','4b08e7a8d3ed6417d6e057a0c4687d99')
-app.set('urlApi','http://api.openweathermap.org/data/2.5/weather?')
+app.set('segredoJwt',process.env.segredoJwt )
+app.set('crypto', process.env.crypto)
+app.set('chaveAcessoTemperatura',process.env.chaveAcessoTemperatura)
+app.set('urlApi',process.env.urlApi)
 
 roteador(app);
 
